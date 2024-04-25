58eed138fa18de2710532581671ec4e0acae9036
Jos242 <73893743+Jos242@users.noreply.github.com>
2024-04-25 01:35:40 -0600

    rip repo
    surely todavia sirve despues de esto

--- backend/gym_landing/gym_landing/settings.py
+++ backend/gym_landing/gym_landing/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         "NAME": "GymDB",
         "USER": "david",
-        "PASSWORD": "david123",
+        "PASSWORD": "1234",
         "HOST": "localhost",
         "PORT": "3306"
     }
