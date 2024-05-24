350997829a42ac86c1a1e22416744cf4070605e8
davidfz7 <david123.dd212@gmail.com>
2024-05-24 08:01:09 -0600

    voy a comer, por eso un commit tam pequenho xD

--- backend/gym_landing/admin_hub/others.py
+++ backend/gym_landing/admin_hub/others.py
@@ -1,7 +1,12 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 def send_email(sender_email, receiver_email, subject, body, smtp_server, smtp_port, login, password):
     # Create the container email message.
     msg = MIMEMultipart()
@@ -23,3 +28,31 @@ def send_email(sender_email, receiver_email, subject, body, smtp_server, smtp_po
     except Exception as e:
         print(f"Failed to send email: {e}")
 
+#sender_email = "david123.dd212@gmail.com"
+#receiver_email = "ahernandez20910@gmail.com"
+#subject = "Test Email"
+#body = "Aja juanki"
+#smtp_server = "smtp.gmail.com"
+#smtp_port = 587
+#login = "david123.dd212@gmail.com"
+#password = "mveq bcxi qvxu fboc"
+#send_email(sender_email, receiver_email, subject, body, smtp_server, smtp_port, login, password)
+
+def signup(request):
+    serializer = UserSerializer(data = request.data)
+    if serializer.is_valid():
+        user = serializer.create(serializer.validated_data)
+        user.save() 
+        user  = User.objects.get(username = request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user = user)
+        return Response({"token": token.key, "user": UserSerializer(instance = user).data}, status= status.HTTP_200_OK)
+    return Response("Couldnt create the user:)", status= status.HTTP_400_BAD_REQUEST) 
+def login(request, format = None):
+   user = get_object_or_404(User, username = request.data['username'])
+   if not user.check_password(request.data['password']):
+       return Response({"detail": "Not found."}, status= status.HTTP_404_NOT_FOUND)
+   token, created = Token.objects.get_or_create(user = user)
+   serializer = UserSerializer(instance = user)
+   return Response({"token": token.key, "user": serializer.data.get("username")})
--- backend/gym_landing/admin_hub/views.py
+++ backend/gym_landing/admin_hub/views.py
@@ -13,7 +13,7 @@ from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
-from .others import send_email
+from .others import send_email, signup, login
 class UserView(APIView):
     parser_classes = (MultiPartParser, FormParser, JSONParser)
 
@@ -21,36 +21,19 @@ class UserView(APIView):
         path = request.path
         print(path)
         if "/signup/" == path:
-            serializer = UserSerializer(data = request.data)
-            if serializer.is_valid():
-                user = serializer.create(serializer.validated_data)
-                user.save() 
-                user  = User.objects.get(username = request.data['username'])
-                user.set_password(request.data['password'])
-                user.save()
-                token = Token.objects.create(user = user)
-                return Response({"token": token.key, "user": UserSerializer(instance = user).data}, status= status.HTTP_200_OK)
-            return Response("Trying to access the signup view", status = status.HTTP_200_OK)
+            return signup(request)
         if "/login/" == path:
-            sender_email = "david123.dd212@gmail.com"
-            receiver_email = "ahernandez20910@gmail.com"
-            subject = "Test Email"
-            body = "Aja juanki"
-            smtp_server = "smtp.gmail.com"
-            smtp_port = 587
-            login = "david123.dd212@gmail.com"
-            password = "mveq bcxi qvxu fboc"
-            send_email(sender_email, receiver_email, subject, body, smtp_server, smtp_port, login, password)
-            return Response("Trying to access the login view", status = status.HTTP_200_OK)
-        return Response("Hello from TestView", status= status.HTTP_200_OK)
+            return login(request)
+        return Response("Hello from UserView", status= status.HTTP_200_OK)
     
 class AuthView(APIView):
     parser_classes = (MultiPartParser, FormParser, JSONParser)
     authentication_classes = [SessionAuthentication, TokenAuthentication]
     permission_classes =  [IsAuthenticated]
     def get(self, request, format=None):
+        print(f'Request auth: {request.auth}')
         content = {
-            'user': str(request.data),  # `django.contrib.auth.User` instance.
+            'user': str(request.user),  # `django.contrib.auth.User` instance.
             'auth': str(request.auth),  # None
         }
         return Response(content)
